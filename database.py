323fe84b799c460413d25ade592e933ca34a824e
Deepak Patidar <dpssvpsl697@email.com>
2025-11-21 15:57:15 +0530

    Switch to Neon PostgreSQL

--- database.py
+++ database.py
@@ -2,20 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLite database file (local)
-SQLALCHEMY_DATABASE_URL = "sqlite:///./indicompute.db"
-# Engine create
+import os
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 
-# Session setup
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Base class for models
 Base = declarative_base()
 
-# Dependency for DB session
 def get_db():
     db = SessionLocal()
     try:
