765f675662057da4c15aa74dfe617393f246e535
Deepak Patidar <dpssvpsl697@email.com>
2025-11-22 08:16:10 +0530

    Fix PostgreSQL setup clean

--- database.py
+++ database.py
@@ -1,13 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
 import os
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
+    DATABASE_URL,
     pool_pre_ping=True
 )
 
