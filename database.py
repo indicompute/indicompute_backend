784b77e7091601825f09064d0f85e938cb7c195f
Deepak Patidar <dpssvpsl697@email.com>
2025-11-23 13:43:23 +0530

    Final stable backend

--- database.py
+++ database.py
@@ -1,24 +1,18 @@
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-if not DATABASE_URL:
-    raise Exception("DATABASE_URL is missing!")
 
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
 )
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
