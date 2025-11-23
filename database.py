9545fd0e1da7db2a499796a58831c10576d927bf
Deepak Patidar <dpssvpsl697@email.com>
2025-11-23 12:13:35 +0530

    Fix

--- database.py
+++ database.py
@@ -1,8 +1,13 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
 import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL is missing!")
 
 engine = create_engine(
     DATABASE_URL,
