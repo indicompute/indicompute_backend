84430d19e4ac0363ababcc26bafbe8c0c02b682e
Deepak Patidar <dpssvpsl697@email.com>
2025-11-26 21:53:04 +0530

    Fix JWT keys

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ logger = log_config.setup_logger()
 # ye line env file load karti hai
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 1440))
 
