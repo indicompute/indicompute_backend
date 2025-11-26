4c2e414250addbbc5c1dd3925d72e48d6a605b80
Deepak Patidar <dpssvpsl697@email.com>
2025-11-26 08:36:13 +0530

    Remove old auth & test files

--- auth_utils.py
+++
@@ -1,9 +0,0 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-def hash_password(plain_pwd: str) -> str:
-    return pwd_context.hash(plain_pwd)
-
-def verify_password(plain_pwd: str, hashed_pwd: str) -> bool:
-    return pwd_context.verify(plain_pwd, hashed_pwd)
